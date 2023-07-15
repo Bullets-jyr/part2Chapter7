3926e0ec084c80feba86973c22b35613bfad343b
Bullets-jyr <jyr@bullets.co.kr>
2023-07-15 15:48:37 +0900

    08. 날씨 위젯 만들기 (1)

---
+++ app/src/main/java/kr/co/bullets/part2chapter7/WeatherAppWidgetProvider.kt
@@ -0,0 +1,40 @@
+package kr.co.bullets.part2chapter7
+
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.widget.RemoteViews
+
+class WeatherAppWidgetProvider : AppWidgetProvider() {
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+
+        // Perform this loop procedure for each App Widget that belongs to this provider
+        appWidgetIds.forEach { appWidgetId ->
+            // Create an Intent to launch ExampleActivity
+            val pendingIntent: PendingIntent = Intent(context, MainActivity::class.java)
+                .let { intent ->
+                    PendingIntent.getActivity(context, 1, intent, PendingIntent.FLAG_IMMUTABLE)
+                }
+
+            // Get the layout for the App Widget and attach an on-click listener
+            // to the button
+            val views: RemoteViews = RemoteViews(
+                context.packageName,
+                R.layout.widget_weather
+            ).apply {
+                setOnClickPendingIntent(R.id.test_text_view, pendingIntent)
+            }
+
+            // Tell the AppWidgetManager to perform an update on the current app widget
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+}
